19a9129acbda64d01981a22a7ac19a2f0df3ad0e
Sbarrachina <sandrabarrachina1978@gmail.com>
2022-05-15 20:53:18 +0200

    fotos carousel

---
+++ emma.js
@@ -0,0 +1,15 @@
+  <script>
+
+$(document).ready(function(){
+$('carousel').carousel();
+
+// function for auto play carousel-slider
+
+setInterval(function(){
+  $('carousel').carousel();
+
+})
+});
+
+
+</script>
